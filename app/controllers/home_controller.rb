d2e88a2863206b6b0bcde107a66fb097af0277b0
imlcl <slackcode@gmail.com>
2013-03-19 10:45:29 +0800

    适应狥的IE8

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
   def index
-    @posts1 = Type.find_by_name('组织结构').posts.limit(5)
-    @posts2 = Type.find_by_name('活动简讯').posts.limit(5)
-    @posts3 = Type.find_by_name('宣传窗口').posts.limit(5)
-    @posts4 = Type.find_by_name('法律公文').posts.limit(5)
+    @posts1 = Type.find_by_name('组织结构').posts.limit(8)
+    @posts2 = Type.find_by_name('活动简讯').posts.limit(8)
+    @posts3 = Type.find_by_name('宣传窗口').posts.limit(8)
+    @posts4 = Type.find_by_name('法律公文').posts.limit(8)
   end
 end
